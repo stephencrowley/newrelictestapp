7cb2ead6a2ea7736365e7de310467da116107e4c
Stephen Crowley <steve.cork@gmail.com>
2018-02-13 15:33:31 +0000

    Update app-ajax.js

--- src/main/webapp/js/app-ajax.js
+++ src/main/webapp/js/app-ajax.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
         $('#userName').blur(function(event) {
-                var name = $('#datepicker').val(); //$('#userName').val()
+                //var name = $('#datepicker').val(); //$('#userName').val()
                 $.get('GetUserServlet', {
                         userName : name
                 }, function(responseText) {
